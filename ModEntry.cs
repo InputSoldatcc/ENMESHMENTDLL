8d336fcbcaf4c6771e9a0f3178208d7bef87fdae
InputSoldatcc <nubroblox8@gmail.com>
2025-06-01 11:49:16 +0100

    Reset ModEntry.cs and remove oboslete EnmeshmentComponent

--- ModEntry.cs
+++ ModEntry.cs
@@ -22,22 +22,6 @@ public class ModEntry : IModEntry
     /// </summary>
     public void OnReady()
     {
-        ImprobabilityDisks.All.Add("Enmeshment", new ZedDisk());
-        ImprobabilityDisks.SetIncompatible("Enmeshment", "tricky", "grunt", "agent", "engineer", "soldat");
-        
-        // Attach plz
-        Game.Main.OnSceneChange.AddListener(Scenes =>
-        {
-            if (Scenes.New != null && (ImprobabilityDisks.IsEnabled("Enmeshment") || ImprobabilityDisks.IsEnabled("PartialEnmeshment")))
-            {
-                MadnessUtils.Delay(1.5f, () => {
-                    foreach (var character in Scenes.New.GetAllComponentsOfType<CharacterComponent>())
-                        AttachEnmesher(character.Entity, character);
-                });
-
-                Scenes.New.OnCreateEntity += DetectCharacter;
-            }
-        });
     }
 
     /// <summary>
@@ -54,13 +38,6 @@ public class ModEntry : IModEntry
     /// <param name="character"></param>
     public static void AttachEnmesher(Entity entity, CharacterComponent character)
     {
-        if (MadnessUtils.FindPlayer(Game.Main.Scene, out var _, out var playerCharacter))
-        {
-            if (playerCharacter != character)
-            {
-                Game.Main.Scene.AttachComponent(character.Entity, new EnmeshmentComponent());
-            }
-        }
     }
 
     /// <summary>
@@ -68,12 +45,5 @@ public class ModEntry : IModEntry
     /// </summary>
     public static void DetectCharacter(Entity entity)
     {
-        _ = MadnessUtils.Delay(1.5f, () =>
-        {
-            if (Game.Main.Scene.TryGetComponentFrom(entity, out CharacterComponent? character))
-            {
-                AttachEnmesher(entity, character);
-            }
-        });
     }
 }
